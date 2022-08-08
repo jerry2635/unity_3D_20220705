9507e7a62da4de83deddcd27a1e9362f4e3bae2f
jerry2635 <105775382+jerry2635@users.noreply.github.com>
2022-08-08 16:31:03 +0800

    updata 對話系統

--- Assets/C#/dialoguesystem.cs
+++ Assets/C#/dialoguesystem.cs
@@ -1,9 +1,13 @@
 using TMPro;
 using UnityEngine;
 using System.Collections;
+using System;
 
 namespace jerry
 {
+    //委派簽名.無傳回.無參數
+    public delegate void DelegateFinishDialogue();
+
     /// <summary>
     /// 對話系統.淡入.更新NPC資料名稱.內容.音效.淡出
     /// </summary>
@@ -21,20 +25,29 @@ namespace jerry
         private AudioSource aud;
         #endregion
 
+        [SerializeField, Header("淡入間隔")]
+        private float intervalFadeIn = 0.1f;
+        [SerializeField, Header("打字間隔")]
+        private float intervalType = 0.05f;
+
         [SerializeField, Header("對話標示")]
         private GameObject gotriangle;
 
-        public NPCdata dataNpc;
+        private NPCdata dataNpc;
 
         private void Awake()
         {
             aud = GetComponent<AudioSource>();
 
-            StartCoroutine(FadeIn());
-
-            textName.text = dataNpc.nameNPC;
-            textContent.text = "";
+            //StartCoroutine(StartDialogue());
         }
+
+        #region 公開資料與方法
+        /// <summary>
+        /// 對話執行與否
+        /// </summary>
+        public bool isDialogue;
+
         #region 協同教學
         ///協同程序需要
         ///引用system.collection
@@ -50,21 +63,49 @@ namespace jerry
         }
         #endregion
 
-        private IEnumerator FadeIn()
+        public IEnumerator StartDialogue(NPCdata _dataNPC,DelegateFinishDialogue callback)
         {
-            for(int i=0;i<10;i++)
+            isDialogue = true;
+
+            dataNpc = _dataNPC;
+
+            textName.text = dataNpc.nameNPC;
+            textContent.text = "";
+
+            yield return StartCoroutine(Fade());
+
+            for (int i=0;i< dataNpc.dataDialogue.Length; i++)
             {
-                groupdialogue.alpha += 0.1f;
-                yield return new WaitForSeconds(0.1f);
+                yield return StartCoroutine(TypeEffect(i));
+
+                while (!Input.GetKeyDown(KeyCode.E))
+                {
+                    yield return null;
+                }
             }
-            StartCoroutine(TypeEffect());
+            StartCoroutine(Fade(false));
+            isDialogue = false;
+            callback(); //執行呼叫程式
+        }
+        #endregion
+
+        private IEnumerator Fade( bool fadeIn=true)
+        {
+            float increase = fadeIn ? 0.1f : -0.1f;
+
+            for (int i = 0; i < 10; i++)
+            {
+                groupdialogue.alpha += increase;
+                yield return new WaitForSeconds(0.3f);
+            }
         }
 
-        private IEnumerator TypeEffect()
+        private IEnumerator TypeEffect(int indexDialogue)
         {
-            aud.PlayOneShot(dataNpc.dataDialogue[0].sound);
+            textContent.text = "";
+            aud.PlayOneShot(dataNpc.dataDialogue[indexDialogue].sound);
 
-            string content = dataNpc.dataDialogue[0].content;
+            string content = dataNpc.dataDialogue[indexDialogue].content;
 
             for (int i = 0; i < content.Length; i++)
             {
---
+++ Assets/C#/npcTip.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+namespace jerry
+{
+    /// <summary>
+    /// 區域進入偵測.顯示畫面.按鍵偵測及啟動
+    /// </summary>
+    public class npcTip : MonoBehaviour
+    {
+        [SerializeField, Header("npc 對話資料")]
+        private NPCdata dataNPC;
+        [SerializeField, Header("NPC攝影機")]
+        private GameObject goCamera;
+
+        /// <summary>
+        /// 提示底圖
+        /// </summary>
+        private Animator aniTi;
+        private string parTipFade = "觸發淡出淡入";
+        private bool isInTrigger;
+        private vegas Vegas;
+        private dialoguesystem dialoguesystem;
+        private Animator ani;
+        private string parDialogue = "對話開關";
+
+        private void Awake()
+        {
+            aniTi = GameObject.Find("圖相框").GetComponent<Animator>();
+            Vegas = FindObjectOfType<vegas>();
+            dialoguesystem = FindObjectOfType<dialoguesystem>();
+            ani = GetComponent<Animator>();
+        }
+        
+        //碰撞事件
+        //其一物件具有rigidbody
+        //勾選trigger.使用ontrigger事件.enter exit stay
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CheckPlayerAndAnimation(other.name,true);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            CheckPlayerAndAnimation(other.name,false);
+        }
+
+        private void Update()
+        {
+            InputKeyAndStartDialogue();
+        }
+        /// <summary>
+        /// 偵測進入或離開.更新動畫
+        /// </summary>
+        private void CheckPlayerAndAnimation(string nameHit,bool _isInTrigger)
+        {
+           if(nameHit=="bigvegas")
+            {
+                isInTrigger = _isInTrigger;
+                aniTi.SetTrigger(parTipFade);
+            }
+        }
+
+        private void InputKeyAndStartDialogue()
+        {
+            if (dialoguesystem.isDialogue) return;
+
+            if (isInTrigger && Input.GetKeyDown(KeyCode.E))
+            {
+                goCamera.SetActive(true);
+                aniTi.SetTrigger(parTipFade);
+                Vegas.enabled = false;
+                try
+                {
+                    ani.SetBool(parDialogue, true);
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+                StartCoroutine(dialoguesystem.StartDialogue(dataNPC,ResetControllerAndCloseCamera));
+            }
+        }
+
+        private void ResetControllerAndCloseCamera()
+        {
+            goCamera.SetActive(false);
+            Vegas.enabled = true;
+            aniTi.SetTrigger(parTipFade);
+            try
+            {
+                ani.SetBool(parDialogue,false);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+}
+
